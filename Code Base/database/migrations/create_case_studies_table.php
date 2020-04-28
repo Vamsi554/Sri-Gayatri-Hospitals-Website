e453422c638dcd20f6c4965e83a25278359243e3
Venkata Vamsi Krishna Bulusu <34600966+Vamsi554@users.noreply.github.com>
2020-04-28 19:14:10 +0530

    Add files via upload

---
+++ Code Base/database/migrations/create_case_studies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCaseStudiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('case_studies', function (Blueprint $table) {
+            $table->bigIncrements('casestudy_id');
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->string('additional_details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('case_studies');
+    }
+}
---
+++ Code Base/database/migrations/create_gallery_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGalleryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gallery', function (Blueprint $table) {
+            $table->bigIncrements('gallery_id');
+            $table->string('img_file_name');
+            $table->string('img_file_name_max');
+            $table->string('file_path');
+            $table->string('additional_details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gallery');
+    }
+}
